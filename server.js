0333baa01ee5441bbb613599eff691cd6d76e476
Jeremiah Currier <jcurrier@slack-corp.com>
2018-11-26 17:00:55 -0800

    Adding files

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+	// return 'foo';
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home page',
+		welcomeMessage: 'Welcome home.'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: `Error with response: ${res.statusCode}`
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
